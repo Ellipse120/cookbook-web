1a621c2e0fc1bb2aeada0d78bc8ad7981676d1f0
Ellipse120 <ellipse120@gmail.com>
2024-04-09 21:48:35 +0800

    feat(九寨沟): deal jsonp problem for latest half month api.

--- server/api/jiuzhaigou.ts
+++ server/api/jiuzhaigou.ts
@@ -23,11 +23,17 @@ type payload = {
   status: string;
 };
 
+declare global {
+  var jsonpAbaTourKc: Function;
+}
+
 export default defineEventHandler(async (event) => {
   const d = getQuery(event);
   const iscenicid = d?.iscenicid || 景点.九寨沟;
   const host = "https://m.abatour.com/chart/dataAnalysis/";
   const today = new Date().toISOString().split("T")[0];
+
+  let a = {};
 
   const promises = Object.entries(RequestMethod).map(([value, key]) =>
     $fetch(`${host}${key}_${iscenicid}.html`, {
@@ -46,6 +52,33 @@ export default defineEventHandler(async (event) => {
       },
     })
   );
+
+  // 未来15天游客预定人数
+  const reqLatestHalfMonthPreorder = $fetch(
+    `https://c.abatour.com/kclistData/futureData_${iscenicid}.html`,
+    {
+      query: {
+        iscenicid,
+        preDays: 3,
+        nextDays: 90,
+        callback: "xxx",
+      },
+      async onResponse({ response }) {
+        globalThis.jsonpAbaTourKc = function (o: any) {
+          a = { ...o };
+        };
+        eval(response._data);
+
+        response._data = {
+          category: "未来15天游客预定人数",
+          data: a,
+        };
+
+        globalThis.jsonpAbaTourKc = () => {};
+      },
+    }
+  );
+  promises.push(reqLatestHalfMonthPreorder);
 
   const responseData = await Promise.allSettled(promises).catch((_) => {
     throw createError({
