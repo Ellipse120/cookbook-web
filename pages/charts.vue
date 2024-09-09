2de80c5be31823782d36796486e45b5a24fd5048
Ellipse120 <ellipse120@gmail.com>
2024-09-09 19:15:29 +0800

    feat(charts): add bubble chart demo

--- pages/charts.vue
+++ pages/charts.vue
@@ -1,3 +1,197 @@
+<script setup lang="ts">
+import type { WombT } from '~/types';
+
+const data = ref<WombT[]>([
+  {
+    id: '1',
+    date: '2021-08-31',
+    value: '59*54',
+    unit: 'mm',
+    womb: {
+      position: '前位',
+      size: {
+        long: 76,
+        width: 70,
+        thickness: 57,
+        endometrium_thickness: 5,
+        cervical_length: 35,
+      }
+    },
+    ovary: {
+      leftSide: {
+        long: 28,
+        width: 16,
+      },
+      rightSide: {
+        long: 30,
+        width: 23,
+      }
+    }
+  },
+  {
+    id: '2',
+    date: '2021-10-19',
+    value: '53*35*42',
+    fixedValue: '53*42',
+    unit: 'mm',
+    womb: {
+      position: '前位',
+      size: {
+        long: 62,
+        width: 58,
+        thickness: 45,
+        endometrium_thickness: 10,
+        cervical_length: 31,
+      }
+    },
+    ovary: {
+      leftSide: {
+        long: 31,
+        width: 20,
+      },
+      rightSide: {
+        long: null,
+        width: null,
+      }
+    }
+  },
+  {
+    id: '3',
+    date: '2022-08-09',
+    value: '39*30',
+    unit: 'mm',
+    womb: {
+      position: '前位',
+      size: {
+        long: 74,
+        width: 65,
+        thickness: 61,
+        endometrium_thickness: 4,
+        cervical_length: 35,
+      }
+    },
+    ovary: {
+      leftSide: {
+        long: 28,
+        width: 18,
+      },
+      rightSide: {
+        long: 27,
+        width: 16,
+      }
+    }
+  },
+  {
+    id: '4',
+    date: '2023-10-05',
+    value: '21*23',
+    unit: 'mm',
+    womb: {
+      position: '前位',
+      size: {
+        long: 56,
+        width: 43,
+        thickness: 53,
+        endometrium_thickness: 11,
+        cervical_length: 28,
+      }
+    },
+    ovary: {
+      leftSide: {
+        long: 26,
+        width: 20,
+      },
+      rightSide: {
+        long: 30,
+        width: 20,
+      }
+    }
+  }
+])
+
+const chartOption = computed(() => ({
+  chart: {
+    type: 'bubble',
+    plotBorderWidth: 1,
+    zooming: {
+      type: 'xy'
+    },
+    // height: (9 / 16 * 80) + '%',
+  },
+
+  legend: {
+    enabled: false
+  },
+
+  title: {
+    text: 'Bubble of womb'
+  },
+
+  subtitle: {
+    text: 'Using faking data'
+  },
+
+  xAxis: {
+    startOnTick: true,
+    endOnTick: true,
+    gridLineWidth: 1,
+    type: 'datetime',
+    title: {
+      text: 'Date'
+    },
+    labels: {
+      format: '{value:%Y-%m-%d}',
+      // rotation: 90,
+    },
+  },
+
+  yAxis: {
+    startOnTick: false,
+    endOnTick: false,
+    title: {
+      text: 'Size'
+    },
+    labels: {
+      format: '{value} mm'
+    },
+    maxPadding: 0.2,
+  },
+
+  tooltip: {
+    formatter(): string {
+      return '日期: <b>' + this.point.rawData.date + '</b> <br> 大小: <b>' + this.point.rawData.value + '</b>';
+    }
+  },
+
+  plotOptions: {
+    series: {
+      dataLabels: {
+        enabled: false,
+        format: '{point.name}',
+      }
+    }
+  },
+
+  series: [{
+    data: data.value.map((o: WombT) => {
+      return {
+        x: new Date(o.date),
+        y: o.womb.size.long,
+        z: (o.fixedValue || o.value).split('*').reduce((acc, curr) => acc * Number(curr), 1),
+        name: o.date,
+        country: o.date,
+        rawData: o,
+      }
+    }),
+    colorByPoint: true
+  }]
+}))
+</script>
+
 <template>
-  <div class="px-4">Chart</div>
+  <div class="px-4">
+    <client-only>
+      <HighchartsWrapper :options="chartOption"/>
+    </client-only>
+  </div>
 </template>
--- types.ts
+++ types.ts
@@ -27,9 +27,43 @@ export type BlogT = {
   title: string;
   link: string;
   views: string;
-};
+}
 
 export type BlogUpdateT = {
   data: BlogT[];
   updateDate: string;
-};
+}
+
+export type WombT = {
+  id: string,
+  date: string | Date,
+  value: string,
+  /** 修正值 */
+  fixedValue?: string,
+  unit: string,
+  womb: {
+    position: string,
+    size: {
+      /** 长 */
+      long: number,
+      /** 宽 */
+      width: number,
+      /** 厚 */
+      thickness: number,
+      /** 内膜厚 */
+      endometrium_thickness: number,
+      /** 宫颈长 */
+      cervical_length: number,
+    }
+  },
+  ovary: {
+    leftSide: {
+      long: number | null,
+      width: number | null
+    },
+    rightSide: {
+      long: number | null,
+      width: number | null
+    },
+  }
+}
