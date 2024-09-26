6941a953c358aa8494c35206c239dbf4c89bed81
Ellipse120 <ellipse120@gmail.com>
2024-09-26 20:53:45 +0800

    feat: add catch all page

---
+++ app/pages/[...slug].vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <q-card>
+      <q-card-section>
+        <q-chip>{{ $route.params.slug }}</q-chip> seems doesn't exist!'
+      </q-card-section>
+
+      <q-card-actions align="center">
+        <NuxtLink to="/">
+          <q-btn
+            type="primary"
+          >
+            返回主页
+          </q-btn>
+        </NuxtLink>
+      </q-card-actions>
+    </q-card>
+  </div>
+</template>
