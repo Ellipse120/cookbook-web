47468d19b5236e761423fef0665f440664f07374
Ellipse120 <ellipse120@gmail.com>
2025-01-01 19:59:26 +0800

    feat(scraps): add hot search news

--- app/pages/scraps.vue
+++ app/pages/scraps.vue
@@ -18,6 +18,18 @@ const fetchData = async () => {
 
   imgData.value = data
 }
+
+const columns = [
+  {
+    name: 'index',
+    label: '#',
+    field: 'index',
+    classes: 'w-20px',
+  },
+  { name: 'heat_score', align: 'center', label: '热搜指数', field: 'heat_score' },
+  { name: 'hotTags', label: 'hotTags', field: 'hotTags' },
+  { name: 'linkurl', label: '详情', field: 'linkurl', headerClasses: 'text-center' },
+]
 </script>
 
 <template>
@@ -47,7 +59,34 @@ const fetchData = async () => {
         :src="imgData?.img"
         alt="tiktok demo png"
         spinner-color="red"
+        class="h-40vh"
       />
+    </div>
+
+    <q-separator />
+
+    <div>
+      <q-table
+        :rows="imgData?.result || []"
+        :columns="columns"
+        row-key="index"
+        bordered
+        flat
+        title="百度热搜"
+        :loading="pending"
+      >
+        <template #body-cell-linkurl="props">
+          <q-td class="text-center text-red-500">
+            <NuxtLink
+              :to="props.row.linkurl"
+              external
+              target="_blank"
+            >
+              {{ props.row.card_title }}
+            </NuxtLink>
+          </q-td>
+        </template>
+      </q-table>
     </div>
   </div>
 </template>
--- server/api/tiktok.post.ts
+++ server/api/tiktok.post.ts
@@ -8,27 +8,44 @@ const uaList = [
   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
 ]
-const oneMinute = 60 * 1000
+const timeout = 30 * 1000
 
 export default defineEventHandler(async (event) => {
   const { link } = await readBody(event)
 
-  const browser = await chromium.launch()
+  const browser = await chromium.launch({
+    headless: true,
+  })
   const context = await browser.newContext({
     userAgent: uaList[random(0, uaList.length - 1)],
   })
 
-  context.setDefaultTimeout(oneMinute)
+  context.setDefaultTimeout(timeout)
   const page = await context.newPage()
 
   await page.goto(link)
 
   const countElement = page.locator('[data-e2e="user-tab-count"]')
-  const count = await countElement.evaluate(node => node.innerText)
+  const count = await countElement.evaluate(node => node.innerText).catch((e) => {
+    createError({
+      statusCode: 500,
+      message: (e as Error).message,
+    })
+  })
 
-  await page.evaluate('[...document.getElementsByClassName(\'screen-mask login-mask-enter-done\')].every(el => el?.remove())')
+  await page.evaluate('[...document.getElementsByClassName(\'screen-mask login-mask-enter-done\'), ...document.querySelectorAll(\'[id^="login-full-panel"]\')].every(el => el?.remove())')
 
-  const buffer = await page.locator('[data-e2e="user-post-list"] > ul').screenshot()
+  const buffer = await page.locator('[data-e2e="user-post-list"] > ul').screenshot().catch((e) => {
+    createError({
+      statusCode: 500,
+      message: (e as Error).message,
+    })
+  })
+
+  // baidu hotsearch
+  const pageOfBaidu = await context.newPage()
+  await pageOfBaidu.goto('https://baidu.com/')
+  const result = await pageOfBaidu.evaluate('window.tempData')
 
   await browser.close()
 
@@ -37,7 +54,8 @@ export default defineEventHandler(async (event) => {
     statusMessage: '获取快照成功',
     data: {
       count,
-      img: 'data:image/png;base64, ' + buffer.toString('base64'),
+      result,
+      img: 'data:image/png;base64, ' + buffer?.toString('base64'),
     },
   }
 })
