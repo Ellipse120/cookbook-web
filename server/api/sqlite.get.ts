2aa10b761500ed327995fc2a5cb490b3348b573d
Ellipse120 <ellipse120@gmail.com>
2025-02-06 17:16:17 +0800

    feat(sqlite): desc user id

--- server/api/sqlite.get.ts
+++ server/api/sqlite.get.ts
@@ -1,13 +1,16 @@
+import { desc } from 'drizzle-orm'
 import { useDb } from '~/utils/db'
 import { users } from '~~/server/database/schema'
 
 export default eventHandler(async () => {
-  const v = await useDb().select().from(users).catch((error) => {
-    throw createError({
-      message: error.message,
-      statusCode: 500,
+  return useDb()
+    .select()
+    .from(users)
+    .orderBy(desc(users.id))
+    .catch((error) => {
+      throw createError({
+        message: error.message,
+        statusCode: 500,
+      })
     })
-  })
-
-  return v
 })
